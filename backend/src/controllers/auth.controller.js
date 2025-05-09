9b5b5f9c7d3ac91405a5fdf2d9cd78c86e1ed7af
Rituparna <srituparnaoffice@gmail.com>
2025-05-09 18:32:40 +0530

    imported {db} from db.js file

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,15 @@
 import bcrypt from "bcryptjs";
+import {db} from "../libs/db.js";
+
 
 export const register = async (req, res) => {
     const { email, password, name } = req.body;
+
+    try {
+        
+    } catch (error) {
+        
+    }
 };
 
 export const login = async (req, res) => {};
