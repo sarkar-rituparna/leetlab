1c8a03a48d039579aa3f4ac167ad5c4a8b5ffe29
Rituparna <srituparnaoffice@gmail.com>
2025-05-09 12:53:08 +0530

    adding other routes' structures

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const authRoutes = express.Router();
 
+authRoutes.post("/register");
+
+authRoutes.post("/login");
+
+authRoutes.post("/logout");
+
+authRoutes.post("/check");
+
 export default authRoutes;
