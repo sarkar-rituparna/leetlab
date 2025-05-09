dd88ded07be52a7cbaf5101c7ed0c5ad1897be23
Rituparna <srituparnaoffice@gmail.com>
2025-05-09 13:18:24 +0530

    controllers' skeleton added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,1 +1,9 @@
 import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
+
+export const check = async (req, res) => {};
