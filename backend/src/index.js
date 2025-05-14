9d989c2ee143d88edd7e24139919479f14ea1de9
Rituparna <srituparnaoffice@gmail.com>
2025-05-14 13:10:54 +0530

    imported & called cookieParser through middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
@@ -8,6 +10,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello Guys welcome to leetlab🔥")
